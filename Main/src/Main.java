0b0c15ac33d08095c117ad28400070783e7f3afc
Marcus McGhee <marcusmcghee27@gmail.com>
2022-06-19 16:07:51 -0400

    intial commit

---
+++ Main/src/Main.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Loan Amount:");
+        double loanAmount = scanner.nextDouble();
+        System.out.println("Loan Amount:" +" "+ loanAmount);
+
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("Enter Loan Term:"+" ");
+        int loanTerm = scanner1.nextInt();
+        System.out.println("Loan Term:"+" "+loanTerm+" "+"Year Fixed");
+
+
+
+        }
+
+
+}
+

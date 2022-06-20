6a925682ce6dace89078861126f70d3f2bd92da7
Marcus McGhee <marcusmcghee27@gmail.com>
2022-06-19 21:27:55 -0400

    first commit

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -14,6 +15,12 @@ public class Main {
         System.out.print("Enter Loan Term:"+" ");
         int loanTerm = scanner1.nextInt();
         System.out.println("Loan Term:"+" "+loanTerm+" "+"Year Fixed");
+
+        Scanner scanner2= new Scanner(System.in);
+        System.out.println("Enter Interest:"+" ");
+        double interestOfLoan = scanner2.nextDouble();
+        NumberFormat fmt1 = NumberFormat.getPercentInstance();
+        System.out.println("Interest:"+" "+fmt1.format(interestOfLoan));
 
 
 

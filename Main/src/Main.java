5d5056eea72fa74a7c12d565675e3089d155bb57
mistermcghee27 <77115137+mistermcghee27@users.noreply.github.com>
2022-06-22 04:45:05 -0400

    changed up the code so I could work with making functions and calling them in main. Still need to the formula of the mortgage payments in.

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -4,31 +4,49 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-
-        //Principal amount $ format.
+    public static double principal(double totalLoanAmount) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter Loan Amount:");
         double loanAmount = scanner.nextDouble();
         NumberFormat fm2 = NumberFormat.getCurrencyInstance();
         System.out.println("Loan Amount:" + " " + fm2.format(loanAmount));
+        return totalLoanAmount;
+    }
 
-        //Loan Term
-        Scanner scanner1 = new Scanner(System.in);
-        System.out.print("Enter Loan Term:" + " ");
-        int loanTerm = scanner1.nextInt();
-        System.out.println("Loan Term:" + " " + loanTerm + " " + "Year Fixed");
-
-        //Interest
+    public static double interest(double annualInterest) {
         Scanner scanner2 = new Scanner(System.in);
         System.out.print("Enter Interest:"+" ");
         double interest = scanner2.nextDouble();
         NumberFormat fmt1 = NumberFormat.getPercentInstance();
         System.out.println("Interest:"+" "+fmt1.format(interest));
+        return annualInterest;
+    }
 
-        System.out.println("Monthly Payments"+" "+loanTerm*12);
-        System.out.println("Payment Amount:"+" "+fm2.format();
+    public static int loanTerm(int yearsFixed) {
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("Enter Loan Term:" + " ");
+        int loanTerm = scanner1.nextInt();
+        System.out.println("Loan Term:" + " " + loanTerm + " " + "Year Fixed");
+        return yearsFixed;
+    }
+
+    public static void main(String[] args) {
+
+        //Principal amount $ format.
+        principal(250000);
+
+        //Loan Term
+        loanTerm(25);
+
+        //Interest
+        interest(.04);
+
+
+        System.out.println("Monthly Payments"+" "+loanTerm(25)*12);
+
+
+        /*Need to figure out how to get the formula for mortgage in the next line!
+        System.out.println("Payment Amount:"+" "+fm2.format();*/
 
     }
 
